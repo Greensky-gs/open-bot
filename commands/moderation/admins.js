efbdd05447c42d7d014a13709d81d1961726389d
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-05-12 19:50:35 +0200

    Create admins.js

---
+++ commands/moderation/admins.js
@@ -0,0 +1,21 @@
+const { Message } = require('discord.js');
+const { reply, classicEmbed } = require('../../assets/functions');
+
+module.exports = {
+    help: {
+        name: 'admins',
+        description: "Affiche la liste des administrateurs du serveur",
+        aliases: ['adminslist'],
+        cooldown: 5,
+        permissions: []
+    },
+    /**
+     * @param {Message} message 
+     */
+    run: async message => {
+        const admins = (await message.guild.members.fetch()).filter(m => m.permissions.has('ADMINISTRATOR')).sort((a, b) => (a.nickname ? a.nickname : a.user.username) - (b.nickname ? b.nickname : b.user.username))
+
+        const embed = classicEmbed(message.author)
+            .setTitle
+    }
+}
