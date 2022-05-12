2ec0963b19d9a051408f7564090fe88b27b54f79
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-05-12 19:15:47 +0200

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -3,6 +3,15 @@ const { Message } = require('discord.js');
 module.exports = {
     help: {
         name: 'nuke',
-        description: "Nuke le salon actuel"
+        description: "Nuke le salon actuel",
+        aliases: [],
+        permissions: ['administrator'],
+        cooldown: 5,
+    },
+    /**
+     * @param {Message} message 
+     */
+    run: message => {
+        
     }
 }
